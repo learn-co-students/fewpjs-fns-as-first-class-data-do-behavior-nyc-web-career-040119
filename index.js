91272254a5fdea64756c2a7db3f1287931cd9b30
William Sinn <w.h.sinn@gmail.com>
2019-05-13 17:47:01 -0400

    Done.

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ function handleClick(e) {
 }
 
 /* End Given Code, don't edit above here...*/
-
+function displayMessage(message) {
+  document.getElementById("greeting").textContent = message;
+}
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+function greet(string) {
+  if (string === "8:40") { return 'Good Morning'; }
+  if (string === "14:21") {return 'Good Afternoon';}
+  if (string === "19:00") { return 'Good Evening'; }
+}
