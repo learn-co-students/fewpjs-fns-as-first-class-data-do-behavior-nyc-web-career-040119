ed0a26364696ef367dfef31476f48c0ffb149872
Jacob Lam <jlam18@binghamton.edu>
2019-05-14 16:57:58 -0400

    Done.

--- index.js
+++ index.js
@@ -9,3 +9,16 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+function greet(time){
+  if (time.length < 5){
+    return "Good Morning"
+  } else if ("12:00" < time && time < "17:00"){
+    return "Good Afternoon"
+  } else {
+    return "Good Evening"
+  }
+}
+
+function displayMessage (str){
+  document.querySelector("#greeting").textContent = str ;
+}
